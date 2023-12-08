174a75054950d56bc6dc21353ad0ee619171ccca
Emopusta <emreduman225@hotmail.com>
2023-12-08 18:09:43 +0300

    init

---
+++ src/DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DataAccessServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<AdessibindenContext>(options => 
+                options.UseNpgsql(
+                    configuration.GetConnectionString("Adessibinden")));
+
+
+            return services;
+        }
+    }
+}
