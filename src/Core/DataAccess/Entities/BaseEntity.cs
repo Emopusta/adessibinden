6dfa83422f8c7aee65ca5abc1f2d044ef1a687fa
Emopusta <emreduman225@hotmail.com>
2023-12-08 18:09:31 +0300

    added base entity

---
+++ src/Core/DataAccess/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Entities
+{
+    public class BaseEntity<TId> : IEntityDates
+    {
+        public TId Id { get; set; }
+        public DateOnly CreatedDate { get; set; }
+        public DateOnly? UpdatedDate { get; set; }
+        public DateOnly? DeletedDate { get; set; }
+
+    }
+}
