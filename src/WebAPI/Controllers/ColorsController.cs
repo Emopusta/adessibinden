0f763d3de37d8e24d6fd8b1c5c1008370ad1d0a0
Emopusta <emreduman225@hotmail.com>
2023-12-11 10:47:07 +0300

    added color controller

---
+++ src/WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Colors.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateColorCommand createColorCommand)
+        {
+            CreatedColorResponse response = await Mediator.Send(createColorCommand);
+
+            return Ok(response);
+        }
+
+    }
+}
