18ff691c350bdec56bc4a9da586b0eb9c192684b
Emopusta <emreduman225@hotmail.com>
2023-12-12 17:04:47 +0300

    version 1 added generic registry and tried to dynamically implement it (needs refactor!)

--- src/DataAccess/DataAccessServiceRegistration.cs
+++ src/DataAccess/DataAccessServiceRegistration.cs
@@ -1,16 +1,15 @@
-﻿using Application.Services.Repositories;
+﻿using Core.Application.GenericRepository;
+using Core.Application.Pipelines.GenericRepository;
+using Core.Application.Rules;
+using Core.DataAccess.Entities;
 using Core.DataAccess.Repositories;
 using DataAccess.Contexts;
-using DataAccess.Repositories;
 using DataAccess.UoW;
+using Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace DataAccess
 {
@@ -25,9 +24,34 @@ namespace DataAccess
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            services.AddScoped<IColorRepository, ColorRepository>();
+            //services.AddScoped<IGenericRepository<Color, Guid>, GenericRepository<Color, Guid>>();
+            services.AddScoped<IGenericRepository<CarBrand, Guid>, GenericRepository<CarBrand, Guid>>();
+
+
+
+            string domainFullName = "Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            var allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(assembly => assembly.FullName.Equals(domainFullName)).ToList();
+            var interfaceGeneric = AppDomain.CurrentDomain.GetAssemblies()[88].DefinedTypes.ToList()[20];
+            var generic = AppDomain.CurrentDomain.GetAssemblies()[30].DefinedTypes.ToList()[13];
+
+            foreach (var loadedAssembly in loadedAssemblies[0].DefinedTypes)
+            {
+                Type type = loadedAssembly.AsType();
+                Type interfaceGenericTypeDefinition = interfaceGeneric.GetGenericTypeDefinition();
+                Type genericTypeDefinition = generic.GetGenericTypeDefinition();
+                interfaceGenericTypeDefinition.MakeGenericType(type, typeof(Guid));
+                genericTypeDefinition.MakeGenericType(type, typeof(Guid));
+
+                services.AddScoped(interfaceGenericTypeDefinition, genericTypeDefinition);
+
+            }
 
             return services;
         }
+
+ 
+
     }
 }
