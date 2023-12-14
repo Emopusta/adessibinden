867911daef03ecda998885a2da888eec6622e1f2
Emopusta <emreduman225@hotmail.com>
2023-12-14 17:31:07 +0300

    added automapper

--- src/Application/ApplicationServiceRegistration.cs
+++ src/Application/ApplicationServiceRegistration.cs
@@ -11,8 +11,8 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-       
 
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
