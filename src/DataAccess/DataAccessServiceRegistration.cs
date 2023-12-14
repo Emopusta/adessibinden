46e1120e2de1534150bdb130fb3f95c615f052f8
Emopusta <emreduman225@hotmail.com>
2023-12-14 16:25:41 +0300

    added Factory for migrations and refactored assemblies

--- src/DataAccess/DataAccessServiceRegistration.cs
+++ src/DataAccess/DataAccessServiceRegistration.cs
@@ -1,12 +1,8 @@
-﻿using Core.Application.GenericRepository;
-using Core.Application.Pipelines.GenericRepository;
-using Core.Application.Rules;
-using Core.DataAccess.Entities;
-using Core.DataAccess.Repositories;
+﻿using Core.DataAccess.Repositories;
 using DataAccess.Contexts;
 using DataAccess.UoW;
-using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -26,12 +22,11 @@ namespace DataAccess
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.RegisterGenericRepositories();
-
-
+  
 
             return services;
         }
-
+        
     private static IServiceCollection RegisterGenericRepositories(this IServiceCollection services)
         {
             var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
@@ -40,15 +35,20 @@ namespace DataAccess
             var coreAssembly = Directory.GetFiles(path, "Core.dll").Select(AssemblyLoadContext.Default.LoadFromAssemblyPath).FirstOrDefault();
             var dataAccessAssembly = Directory.GetFiles(path, "DataAccess.dll").Select(AssemblyLoadContext.Default.LoadFromAssemblyPath).FirstOrDefault();
 
+            if (domainAssembly == null || coreAssembly == null || dataAccessAssembly == null) throw new Exception("Assemblies could not found.");
+
             var iGenericRepository = coreAssembly.DefinedTypes.FirstOrDefault(p => p.Name.Equals("IGenericRepository`1"));
             var genericRepository = dataAccessAssembly.DefinedTypes.FirstOrDefault(p => p.Name.Equals("GenericRepository`1"));
 
+            if (iGenericRepository == null || genericRepository == null) throw new Exception("GenericRepository Assemblies could not found.");
 
             foreach (var domain in domainAssembly.DefinedTypes)
             {
                 Type domainEntityType = domain.AsType();
                 Type iGenericTypeDefinition = iGenericRepository.GetGenericTypeDefinition();
                 Type genericTypeDefinition = genericRepository.GetGenericTypeDefinition();
+
+                if (domainEntityType == null) { continue; }
 
                 iGenericTypeDefinition.MakeGenericType(domainEntityType);
                 genericTypeDefinition.MakeGenericType(domainEntityType);
@@ -60,5 +60,22 @@ namespace DataAccess
             return services;
         }
 
+    }
+
+    public class DbContextDesignFactory : IDesignTimeDbContextFactory<AdessibindenContext>
+    {
+        
+
+        public AdessibindenContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AdessibindenContext>();
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("Adessibinden"));
+
+            return new AdessibindenContext(optionsBuilder.Options);
+        }
     }
 }
