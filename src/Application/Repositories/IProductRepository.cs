7532a5f5d24bbd55dd712a5e45d37b9f1c69270e
Emopusta <emreduman225@hotmail.com>
2024-03-22 13:20:19 +0300

    Add Custom Product Repository

---
+++ src/Application/Repositories/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Core.DataAccess.Repositories;
+using Domain.Models;
+
+namespace Application.Repositories;
+
+public interface IProductRepository : IAsyncRepository<Product>, IRepository<Product>, IBaseCustomRepository
+{
+}
---
+++ src/DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Repositories;
+using Core.DataAccess.Repositories;
+using DataAccess.Contexts;
+using Domain.Models;
+
+namespace DataAccess.Repositories;
+
+public class ProductRepository : EfRepositoryBase<Product, AdessibindenContext>, IProductRepository
+{
+    public ProductRepository(AdessibindenContext context) : base(context)
+    {
+    }
+}
