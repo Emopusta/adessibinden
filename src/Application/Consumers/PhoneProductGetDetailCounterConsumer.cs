81b875552cb659c8cb1a1d5b9a82ef02c05ad596
Emopusta <emreduman225@hotmail.com>
2024-03-28 13:09:14 +0300

    Update test CAP RabbitMQ implementation v0.1

--- src/Application/Consumers/PhoneProductGetDetailCounterConsumer.cs
+++ src/Application/Consumers/PhoneProductGetDetailCounterConsumer.cs
@@ -1,12 +1,43 @@
-﻿using DotNetCore.CAP;
+﻿using Core.Application.GenericRepository;
+using Core.DataAccess.UoW;
+using Domain.Models;
+using DotNetCore.CAP;
 using System.Text.Json;
+using System.Transactions;
 
 namespace Application.Consumers;
 public class PhoneProductGetDetailCounterConsumer : ICapSubscribe
 {
-    [CapSubscribe("phone_product_details_queue_cap")]
-    public static void Consumer(JsonElement phoneProduct)
+    private readonly IGenericRepository<InteractionCount> _interactionCountRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PhoneProductGetDetailCounterConsumer(IGenericRepository<InteractionCount> interactionCountRepository, IUnitOfWork unitOfWork)
     {
+        _interactionCountRepository = interactionCountRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    [CapSubscribe("phone_product_details_queue_cap")]
+    public async Task ConsumerAsync(JsonElement phoneProduct, CancellationToken cancellationToken)
+    {
+
+        using TransactionScope transactionScope = new(TransactionScopeAsyncFlowOption.Enabled);
+        try
+        {
+            var interaction = await _interactionCountRepository.GetAsync(p => p.Name == "phone_product_details_queue_cap", cancellationToken: cancellationToken);
+            interaction!.Count++;
+            var result = await _interactionCountRepository.UpdateAsync(interaction);
+            await _unitOfWork.SaveAsync(cancellationToken);
+            Console.WriteLine(result);
+            throw new Exception("Test Exception");
+            transactionScope.Complete();
+        }
+        catch (Exception)
+        {
+            transactionScope.Dispose();
+            throw;
+        }
+      
         Console.WriteLine(phoneProduct);
     }
 
