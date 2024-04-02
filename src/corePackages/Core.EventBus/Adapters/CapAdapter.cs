b18ddb0885b8a19cb6b1e94a6f1b724560dfe8db
Emopusta <emreduman225@hotmail.com>
2024-04-02 15:02:11 +0300

    Add logging to Cap Adapter

--- src/corePackages/Core.EventBus/Adapters/CapAdapter.cs
+++ src/corePackages/Core.EventBus/Adapters/CapAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using Core.EventBus.Messages;
+using Core.Logging.Serilog;
 using DotNetCore.CAP;
 
 namespace Core.EventBus.Extensions;
@@ -6,14 +7,18 @@ namespace Core.EventBus.Extensions;
 public class CapAdapter : ICapAdapter
 {
     private readonly ICapPublisher _capPublisher;
+    private readonly ILogger _logger;
 
-    public CapAdapter(ICapPublisher capPublisher)
+    public CapAdapter(ICapPublisher capPublisher, ILogger logger)
     {
         _capPublisher = capPublisher;
+        _logger = logger;
     }
 
     public async Task PublishAsync<T>(T message, CancellationToken cancellationToken) where T : MessageBase
     {
+        _logger.Information($"Publishing Async message: {typeof(T).Name}");
+
         await _capPublisher.PublishAsync(typeof(T).Name, contentObj: message, cancellationToken: cancellationToken);
     }
 }
